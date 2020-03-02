014750b0c9c78e9681ab44f85007cd8b54f8ca65
vcoelho23 <coelhorails@gmail.com>
2020-03-02 11:09:35 -0300

    adicionado os pre compiles dos assets backoffice.js,backoffice.css, site.css e site.hs

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,9 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+Rails.application.config.assets.precompile += %w( tema_fundo.css )
+Rails.application.config.assets.precompile += %w( site.css )
+Rails.application.config.assets.precompile += %w( site.js )
+Rails.application.config.assets.precompile += %w( backoffice.css )
+Rails.application.config.assets.precompile += %w( backoffice.js)
